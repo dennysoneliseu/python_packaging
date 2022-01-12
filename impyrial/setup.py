69161236ed200fc9cecd2df3aa4c512a2104eb9f
dennysoneliseu <dennyson.eliseu@gmail.com>
2022-01-12 00:55:43 -0300

    Developing dependencies and requirements

--- impyrial/setup.py
+++ impyrial/setup.py
@@ -8,6 +8,8 @@ setup(
     description="A package for converting imperial lengths and weights.",
     name="impyrial",
     version="0.1.0",
-    packages=find_packages(include=["impyrial", "impyrial.*"])
+    packages=find_packages(include=["impyrial", "impyrial.*"]),
+    version="0.1.0",
+    install_requires=["numpy>=1.10", "pandas"],
 )
 
